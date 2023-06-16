ff60ca08e5e036206e5df33f4a6c03438a14395c
FadyBengoufa <fady.bengoufa@gmail.com>
2023-06-16 21:43:25 +0100

    change url for the backend

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://technotes-api.onrender.com",
+  baseUrl: "https://technotes-api-dcga.onrender.com",
   credentials: "include", // this is to be able to send the cookie
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
